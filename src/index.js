5d6064e9398cefbb7a56f3678e83049773687b8a
German <german.a.almonte@gmail.com>
2022-06-02 09:30:26 -0400

    added gravity to bird

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const config = {
   // WebGL js api for rendering 2d and 3d graphics.
@@ -7,22 +7,30 @@ const config = {
   height: 600,
   physics: {
   // Arcade physics plugin, manages physics simulations
-    default: 'arcade'
+    default: 'arcade',
+    arcade: {
+      gravity: {y:200}
+    }
   },
   scene:{
     preload,
     create,
+    update
   }
 }
-
+let bird = null;
 // Loading assets
 function preload(){
   this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png')
+  this.load.image('bird', 'assets/bird.png');
 }
 // Initializing instances of objects in memory
 function create(){
-  this.add.image(0,0,'sky').setOrigin(0)
-  this.add.sprite(config.width*0.1,config.height/2,'bird').setOrigin(0)
+  this.add.image(0,0,'sky').setOrigin(0);
+  bird = this.physics.add.sprite(config.width*0.1,config.height/2,'bird').setOrigin(0);
 }
-new Phaser.Game(config)
+
+// 60 fps / 60 times per second updates
+function update(time,delta){
+}
+new Phaser.Game(config);

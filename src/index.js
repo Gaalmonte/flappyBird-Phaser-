bc81d3922d41b5cd6d42d12020ed829af02df6ee
German <german.a.almonte@gmail.com>
2022-06-03 12:17:04 -0400

    playing around with bird velocity

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const config = {
   // Arcade physics plugin, manages physics simulations
     default: 'arcade',
     arcade: {
-      gravity: {y:200}
+      // gravity: {y:200}
+      debug:true
     }
   },
   scene:{
@@ -18,19 +19,29 @@ const config = {
     update
   }
 }
-let bird = null;
+
 // Loading assets
 function preload(){
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
 }
+const VELOCITY = 200;
+let bird = null;
+let totalDelta = null;
 // Initializing instances of objects in memory
 function create(){
   this.add.image(0,0,'sky').setOrigin(0);
   bird = this.physics.add.sprite(config.width*0.1,config.height/2,'bird').setOrigin(0);
+  bird.body.velocity.x = VELOCITY;
 }
 
 // 60 fps / 60 times per second updates
+
 function update(time,delta){
+  if(bird.x >= config.width - bird.width){
+    bird.body.velocity.x = -VELOCITY;
+  } else if (bird.x <= 0){
+    bird.body.velocity.x = VELOCITY;
+  }
 }
 new Phaser.Game(config);
